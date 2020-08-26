1584a692f610372a7743c8c8cbb8893ca85384b0
shiven217 <shivender.kvs@gmail.com>
2020-08-26 23:29:23 +0530

    added portfolio

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+window.addEventListener('scroll',function(){
+
+    var header=document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY>0);
+
+});
+
+function toggeleMenu(){
+    var menuToggle=document.querySelector('.toggle');
+    var menu=document.querySelector('.menu');
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+}
+
+
